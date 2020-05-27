0fe8b25cdec3b542f4fd0184a2c3b74cc7b595b5
Long The Xuan Nguyen <longntx@tech.est-rouge.com>
2020-05-27 17:56:22 +0700

    Testing delete caches

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,21 @@
 const VERSION = 6.1;
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+workbox.setConfig({ debug: false, skipWaiting: false, clientsClaim: true });
+
+workbox.routing.registerRoute(
+  new RegExp('\\.(?:html|js|json)$'),
+  new workbox.strategies.CacheFirst({
+    cacheName: `${VERSION}`,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({ maxAgeSeconds: 86400 * 7 * 30, purgeOnQuotaError: false }),
+      new workbox.broadcastUpdate.BroadcastUpdatePlugin('update-myCache')
+    ]
+  }),
+  'GET'
+);
+
 self.addEventListener('message', event => {
   if (!event.data) {
     return;
@@ -14,6 +30,16 @@ self.addEventListener('message', event => {
   }
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(new Promise(resolve => setTimeout(resolve, 10 * 1000)).then(() => console.log('Activate done')));
-});
+self.addEventListener('activate', event => event.waitUntil(
+  caches.keys().then(function(cacheNames) {
+    return Promise.all(
+      cacheNames
+        .filter(cacheName => true)
+        .map(cacheName => {
+          return caches.delete(cacheName);
+        })
+    )
+      .then(() => self.clients.claim())
+      .catch(() => console.log('error'));
+  })
+));
